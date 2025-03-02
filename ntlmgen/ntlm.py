71b38e50362c82e821e25d7d72e62f3cada20211
Robert Gilbert <3596912+amroot@users.noreply.github.com>
2025-03-01 16:16:32 -0800

    Closes #1

--- ntlmgen/ntlm.py
+++ ntlmgen/ntlm.py
@@ -6,5 +6,11 @@ def NTLM(password):
     """Returns an NTLM hash in hex from string
     Param password (str): the value to hash
     """
-
-    return new_hash('md4', password.encode('utf-16le')).hexdigest()
+    try:
+        ntlm_hash = new_hash('md4', password.encode('utf-16le')).hexdigest()
+    except ValueError as e:
+        print(f'[!] {e}')
+        print('[i] You may need to enable the insecure md4 cipher in your openssl.cnf')
+        print('[i] Reference: https://github.com/amroot/ntlm_gen/issues/1#issuecomment-2692478764')
+        exit(1)
+    return ntlm_hash
